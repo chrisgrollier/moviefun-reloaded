82e15e070a73989360f1cfc286ebdd6ca096f0fc
chrisgrollier <cgrollier@free.fr>
2019-11-30 18:13:45 +0100

    Added @RefreshScope

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -3,9 +3,11 @@ package org.superbiz.moviefun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RefreshScope
 public class Application {
 
     public static void main(String[] args) {
